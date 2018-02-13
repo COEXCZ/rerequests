c292c1ddb6a06a4c910c36013c40269e19de733f
Jan Češpivo <jan.cespivo@gmail.com>
2018-02-13 12:35:26 +0100

    setup and manifest file

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages=['rerequests'],
     url='https://github.com/COEXCZ/rerequests',
     license='Apache License 2.0',
-    author='Jan Češpivo',
-    author_email='',
+    author='Jan Češpivo (COEX)',
+    author_email='support@coex.cz',
     description='Wrapper around requests library with automatic retry ability',
     install_requires=['requests']
 )
